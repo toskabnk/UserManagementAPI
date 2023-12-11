7f821d493c80cc78b3dc173c35d0e37431cef8d5
Antonio <santiagodelarosaantonio@gmail.com>
2023-12-11 13:36:13 +0100

    Seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -54,24 +54,5 @@ class DatabaseSeeder extends Seeder
         $org->client_id = $client->id;
         $org->description = 'Organizacion por defecto';
         $org->save();
-
-        //San Valero
-        $user = new User();
-        $user->email = 'admin@svalero.com';
-        $user->password = Hash::make('1234');
-        $user->save();
-
-        $client = new Client();
-        $client->name = 'San Valero';
-        $client->user_id = $user->id;
-        $client->save();
-        $client->roles()->attach($admin->id);
-        $client->roles()->attach($student->id);
-
-        $org = new Organization();
-        $org->name = 'San Valero';
-        $org->client_id = $client->id;
-        $org->description = 'Centro San Valero es un centro de enseñanza concertada y privada. Imparte formación de ESO, Bachillerato, FP Básica, Grado Medio, Grado Superior, Cursos de especialización, cursos de formación para el empleo, entre otras. La Fundación San Valero es una Obra Diocesana de carácter no lucrativo al servicio de las personas y de la sociedad.';
-        $org->save();
     }
 }
